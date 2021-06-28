5d916e1cf2c6bb4107d97d7035a36f5295f81e31
Evangelia0 <56823552+Evangelia0@users.noreply.github.com>
2021-06-28 21:26:21 +0300

    Add files via upload

---
+++ UI/most_visited_areas(60+).sql
@@ -0,0 +1,7 @@
+SELECT COUNT(*), Service_ID
+FROM Client
+INNER JOIN service_charge ON Client.NFC_ID = service_charge.NFC_ID
+WHERE Client.date_of_birth BETWEEN date_add(CURDATE(), INTERVAL -100 YEAR) AND date_add(CURDATE(), INTERVAL -60 YEAR) 
+GROUP BY Service_ID
+ORDER BY COUNT(*) DESC
+;
