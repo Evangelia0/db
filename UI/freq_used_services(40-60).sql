67c863b3ed74e4fd26f855bff284c37246d30a1e
Evangelia0 <56823552+Evangelia0@users.noreply.github.com>
2021-06-28 21:52:40 +0300

    Add files via upload

---
+++ UI/freq_used_services(40-60).sql
@@ -0,0 +1,10 @@
+select count(*), dist_combs.Service_ID
+FROM 
+(SELECT distinct service_charge.NFC_ID, service_charge.Service_ID
+from service_charge
+INNER JOIN Client on Client.NFC_ID = service_charge.NFC_ID
+WHERE Client.date_of_birth BETWEEN date_add(CURDATE(), INTERVAL -60 YEAR) AND date_add(CURDATE(), INTERVAL -40 YEAR) 
+) as dist_combs
+group by dist_combs.Service_ID
+order by count(*) desc
+ ;
